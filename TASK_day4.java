85d276461b7221a63667e97fd892539df9852ca4
shanjunagaraj <139363758+shanjunagaraj@users.noreply.github.com>
2025-06-30 21:18:20 +0530

    Update TASK_day4.java

--- TASK_day4.java
+++ TASK_day4.java
@@ -1,3 +1,29 @@
+// TASK1:
+  import java.util.Scanner;
+
+public class DivideByZeroHandling {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter numerator: ");
+        int numerator = sc.nextInt();
+
+        System.out.print("Enter denominator: ");
+        int denominator = sc.nextInt();
+
+        try {
+            int result = numerator / denominator;
+            System.out.println("Result: " + result);
+        } catch (ArithmeticException e) {
+            System.out.println("Error: Cannot divide by zero.");
+        }
+
+        System.out.println("Program continues...");
+        sc.close();
+    }
+}
+
+//TASK2:
 import java.io.*;
 
 public class SimpleBufferedReader {
@@ -41,7 +67,7 @@ public class SimpleFileWrite {
     }
 }
 
-MAIN PROGRAM:
+//MAIN PROGRAM:
 
 import java.io.*;
 
