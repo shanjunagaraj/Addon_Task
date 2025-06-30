56d1205b98547992244b193f7c24f3636b1ffed1
shanjunagaraj <139363758+shanjunagaraj@users.noreply.github.com>
2025-06-30 21:25:05 +0530

    Update TASK_day6.java

--- TASK_day6.java
+++ TASK_day6.java
@@ -1,4 +1,4 @@
-TASK 1:
+// TASK 1:
 
 class MyThread extends Thread {
     public void run() {
@@ -23,7 +23,7 @@ public class Main {
 }
 
 
-TASK 2:
+// TASK 2:
 
 class MessagePrinter implements Runnable {
     public void run() {
@@ -45,7 +45,7 @@ public class MultiThreadExample {
     }
 }
 
-TASK 3:
+// TASK 3:
 
 
 class ThreadA extends Thread {
@@ -88,6 +88,64 @@ public class SleepJoinDemo {
 
         tA.start();
         tB.start();
+    }
+}
+
+// TASK 4:
+
+class TicketCounter {
+    private int seatsAvailable = 10;
+
+    // Synchronized method to book ticket
+    public void bookTicket(String name, int numberOfSeats) {
+        synchronized (this) {
+            System.out.println(name + " is trying to book " + numberOfSeats + " seat(s).");
+            
+            if (numberOfSeats <= seatsAvailable) {
+                System.out.println("Seats available. Processing booking for " + name + "...");
+
+                try {
+                    Thread.sleep(2000); // Simulate delay
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                seatsAvailable -= numberOfSeats;
+                System.out.println("Booking successful for " + name + ". Seats remaining: " + seatsAvailable);
+            } else {
+                System.out.println("Booking failed for " + name + ". Not enough seats. Seats remaining: " + seatsAvailable);
+            }
+        }
+    }
+}
+
+class User extends Thread {
+    private TicketCounter counter;
+    private String name;
+    private int seatsNeeded;
+
+    public User(TicketCounter counter, String name, int seatsNeeded) {
+        this.counter = counter;
+        this.name = name;
+        this.seatsNeeded = seatsNeeded;
+    }
+
+    public void run() {
+        counter.bookTicket(name, seatsNeeded);
+    }
+}
+
+public class TicketBookingSimulation {
+    public static void main(String[] args) {
+        TicketCounter counter = new TicketCounter();
+
+        User u1 = new User(counter, "Alice", 4);
+        User u2 = new User(counter, "Bob", 5);
+        User u3 = new User(counter, "Charlie", 3);
+
+        u1.start();
+        u2.start();
+        u3.start();
     }
 }
 
